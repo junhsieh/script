b3366b4c83d6bf175743b343e65eebf8496fcfa0
Jun Hsieh <jun.github@ijun.org>
2014-12-28 01:40:08 -0800

    add cp_vim_setting_to_users.sh

---
+++ cp_vim_setting_to_users.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+srcUser="git"
+srcDir=/home/${srcUser}
+destUser="jun root"
+destDir=""
+vimbakDir=""
+userGroup=""
+
+for du in $destUser; do
+  if [ "${du}" = "root" ]; then
+    destDir=/${du}
+  else
+    destDir=/home/${du}
+  fi
+
+  vimbakDir=${destDir}/.vimbak
+  userGroup=`id -gn ${du}`
+
+  /bin/rm -r ${destDir}/.vim
+  /bin/rm -r ${destDir}/.vimrc
+  /bin/cp -r ${srcDir}/.vim ${destDir}
+  /bin/cp -r ${srcDir}/.vimrc ${destDir}
+
+  if [ ! -d ${vimbakDir} ]; then
+    /bin/mkdir -p ${vimbakDir}
+  fi
+
+  /usr/bin/find ${destDir} -name '.vim*' -print0 | xargs -0 -I {} /usr/sbin/chown ${du}:${userGroup} {}
+  /bin/chmod 750 ${vimbakDir}
+done
+
